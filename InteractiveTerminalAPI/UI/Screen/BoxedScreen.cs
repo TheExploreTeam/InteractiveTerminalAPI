dae5ec8728e7e8efd4b032dddd86834f2eddade9
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-05-27 17:05:51 +0100

    Fixed issue with title being too big and breaking the screen

--- InteractiveTerminalAPI/UI/Screen/BoxedScreen.cs
+++ InteractiveTerminalAPI/UI/Screen/BoxedScreen.cs
@@ -14,27 +14,29 @@ namespace InteractiveTerminalAPI.UI.Screen
         public ITextElement[] elements;
         public virtual string GetText(int availableLength)
         {
+            string title = Title;
+            if (Title.Length > availableLength - 6) title = title.Substring(0, availableLength - 6);
             StringBuilder sb = new StringBuilder();
             sb.AppendLine().AppendLine();
             sb.Append(new string(APIConstants.WHITE_SPACE, 1))
                 .Append(APIConstants.TOP_LEFT_TITLE_CORNER)
-                .Append(new string(APIConstants.HORIZONTAL_TITLE_LINE, Title.Length + 2))
+                .Append(new string(APIConstants.HORIZONTAL_TITLE_LINE, title.Length + 2))
                 .Append(APIConstants.TOP_RIGHT_TITLE_CORNER)
                 .AppendLine();
             sb.Append(APIConstants.TOP_LEFT_CORNER)
                 .Append(APIConstants.CONNECTING_TITLE_LEFT)
                 .Append(APIConstants.WHITE_SPACE)
-                .Append(Title)
+                .Append(title)
                 .Append(APIConstants.WHITE_SPACE)
                 .Append(APIConstants.CONNECTING_TITLE_RIGHT)
-                .Append(new string(APIConstants.HORIZONTAL_LINE, availableLength - 6 - Title.Length))
+                .Append(new string(APIConstants.HORIZONTAL_LINE, availableLength - 6 - title.Length))
                 .Append(APIConstants.TOP_RIGHT_CORNER)
                 .AppendLine();
             sb.Append(APIConstants.VERTICAL_LINE)
                 .Append(APIConstants.BOTTOM_LEFT_TITLE_CORNER)
-                .Append(new string(APIConstants.HORIZONTAL_TITLE_LINE, Title.Length + 2))
+                .Append(new string(APIConstants.HORIZONTAL_TITLE_LINE, title.Length + 2))
                 .Append(APIConstants.BOTTOM_RIGHT_TITLE_CORNER)
-                .Append(new string(APIConstants.WHITE_SPACE, availableLength - 6 - Title.Length))
+                .Append(new string(APIConstants.WHITE_SPACE, availableLength - 6 - title.Length))
                 .Append(APIConstants.VERTICAL_LINE)
                 .AppendLine();
             for (int i = 0; i < elements.Length; i++)
